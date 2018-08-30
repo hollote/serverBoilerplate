544de5c5f238605628760446dc2f8741704eb61f
Hollote <ukrainetsvik@gmail.com>
2018-08-30 09:40:41 +0300

    move env variables to docker compose, add persistent storage for db (redis in progress)

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@
 
 module.exports = {
   redisStore: {
-    url: process.env.REDIS_STORE_URI || '//localhost:6379',
-    secret: process.env.REDIS_STORE_SECRET || 'secret'
+    url: process.env.REDIS_STORE_URI,//  || '//localhost:6379',
+    secret: process.env.REDIS_STORE_SECRET// || 'secret'
   },
   mongoDB: {
-    url: 'mongodb://mongo:27017/baseNodeJs'
+    url: process.env.MONGO_STORE_URI// || 'mongodb://localhost:27017/baseNodeJs'
     //TODO: move to docker compose environment variables configuration
   }
 };
