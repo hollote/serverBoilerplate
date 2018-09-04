66115c1233cada0ab7d913eb53ce503acc2fd8b5
Hollote <ukrainetsvik@gmail.com>
2018-09-04 22:40:15 +0300

    move server part of application to /server folder + change docker files

---
+++ server/config/database.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  redisStore: {
+    url: process.env.REDIS_STORE_URI || '//localhost:6379',
+    secret: process.env.REDIS_STORE_SECRET || 'secret'
+  },
+  mongoDB: {
+    url: process.env.MONGO_STORE_URI || 'mongodb://localhost:27017/baseNodeJs'
+  }
+};
